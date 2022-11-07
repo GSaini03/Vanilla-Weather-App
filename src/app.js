969d3db51d7f7d9a3768d278ca4be981f07a53a7
GSaini03 <116730590+GSaini03@users.noreply.github.com>
2022-11-07 18:31:08 +0100

    Added HTML forcast template in JS

--- src/app.js
+++ src/app.js
@@ -24,6 +24,38 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+  <div class="col-2">
+  <div class="weather-forecast-date">${day}</div>
+  
+  <img
+  src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+  alt=""
+  width="44"
+  />
+  
+  <div class="weather-forecast-temperatures">
+  <span class="weather-forecast-temperature-max"> 18° </span>
+  <span class="weather-forecast-temperature-min"> 12° </span>
+  </div>
+  
+  </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   //document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
@@ -97,3 +129,5 @@ let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
 search("Paris");
+
+displayForecast();
